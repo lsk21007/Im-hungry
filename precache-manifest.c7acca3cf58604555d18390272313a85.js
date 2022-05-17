4f1e3be6aa3344b597670d82b8f6626ca8b3e98e
Lawrence Liu <lsk21007@gmail.com>
2022-05-16 22:53:47 -0700

    Updates

---
+++ precache-manifest.c7acca3cf58604555d18390272313a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2560c96d44e24f31dc0e88e6b4bbd9c",
+    "url": "/Im-hungry/index.html"
+  },
+  {
+    "revision": "9756669f65edc6d066df",
+    "url": "/Im-hungry/static/css/main.c57470de.chunk.css"
+  },
+  {
+    "revision": "516735771c45274755a1",
+    "url": "/Im-hungry/static/js/2.271fe9ab.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Im-hungry/static/js/2.271fe9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9756669f65edc6d066df",
+    "url": "/Im-hungry/static/js/main.afdd3cc4.chunk.js"
+  },
+  {
+    "revision": "c0e27d15bfc163e69936",
+    "url": "/Im-hungry/static/js/runtime-main.9275a3ed.js"
+  }
+]);
